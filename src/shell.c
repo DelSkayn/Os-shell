647ca7054298b47b3c2249e8e4f25fc3b009438a
DelSkayn <yoko.minsky@gmail.com>
2016-02-24 16:37:33 +0100

    Things dont want to wait but it works mostly.

--- src/shell.c
+++ src/shell.c
@@ -7,55 +7,54 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #include "shell.h"
 
 const size_t SIZE_INCREASE = 40;
 const size_t PATH_BUFFER_SIZE = 1024;
 const size_t MAX_TOKENS = 128;
-
 /*
  * A struct for containing the command
  */
 struct CommandBuffer{
-	char * command_buffer;
-	size_t size;
+    char * command_buffer;
+    size_t size;
 };
-
 struct CommandBuffer command_buffer_factory(){
-	struct CommandBuffer cb;
-	cb.command_buffer = NULL;
-	cb.size = 0;
-	return cb;
+    struct CommandBuffer cb;
+    cb.command_buffer = NULL;
+    cb.size = 0;
+    return cb;
 }
 
 void delete_command_buffer(struct CommandBuffer * cmb){
-	free(cmb->command_buffer);
+    free(cmb->command_buffer);
 }
 
 bool get_command(struct CommandBuffer * cb,FILE * in){
-	if(cb->command_buffer){//free remains
-		free(cb->command_buffer);
-		cb->command_buffer = NULL;
-		cb->size = 0;
-	}
-	size_t itt = 0;
-	for(;;){
-		cb->command_buffer = realloc(cb->command_buffer,cb->size+sizeof(char)*SIZE_INCREASE);
-		cb->size += SIZE_INCREASE;
-		//init memory
-		for(size_t i = SIZE_INCREASE*itt;i < cb->size;++i){
-			cb->command_buffer[i] = fgetc(in);
-			if(cb->command_buffer[i] == '\n'){
-				cb->command_buffer[i] = '\0';
-				return true;
-			}else if(cb->command_buffer[i] == EOF){
-				printf("Error: Eof during before end reading\n");
-				return false;
-			}
-		}
-		++itt;
-	}
+    if(cb->command_buffer){//free remains
+        free(cb->command_buffer);
+        cb->command_buffer = NULL;
+        cb->size = 0;
+    }
+    size_t itt = 0;
+    for(;;){
+        cb->command_buffer = realloc(cb->command_buffer,cb->size+sizeof(char)*SIZE_INCREASE);
+        cb->size += SIZE_INCREASE;
+        //init memory
+        for(size_t i = SIZE_INCREASE*itt;i < cb->size;++i){
+            cb->command_buffer[i] = fgetc(in);
+            if(cb->command_buffer[i] == '\n'){
+                cb->command_buffer[i] = '\0';
+                return true;
+            }else if(cb->command_buffer[i] == EOF){
+                printf("Error: Eof during before end reading\n");
+                return false;
+            }
+        }
+        ++itt;
+    }
 }
 
 void removeChar(char *str, char garbage) {
@@ -69,76 +68,146 @@ void removeChar(char *str, char garbage) {
 }
 
 int parse_command(char ** buff, char * src, const char * delim){
-        buff[0] = src;
-        for(size_t i = 1;i < MAX_TOKENS;i++){
-            buff[i] = strstr(buff[i-1],delim);
+    buff[0] = src;
+    for(size_t i = 1;i < MAX_TOKENS;i++){
+        buff[i] = strstr(buff[i-1],delim);
+        //test for end of string
+        if(!buff[i])
+            return i;
+        //test for escape characters 
+        while(*(buff[i]-1) == '\\'){
+            buff[i] = strstr(buff[i]+1,delim);
             //test for end of string
             if(!buff[i])
-                return i;
-            //test for escape characters 
-            while(*(buff[i]-1) == '\\'){
-                buff[i] = strstr(buff[i]+1,delim);
-            //test for end of string
-                if(!buff[i])
-                    removeChar(buff[i-1],'\\');
-                    return i;
-            }
-            //replace the space with a endstring
-            *buff[i] = '\0';
-            //move over the endstring
-            buff[i] += 1;
-            //test if string is empty
-            if(strcmp(buff[i-1],"") == 0){
-                buff[i-1] = buff[i];
-                i -= 1;
-            }
-            removeChar(buff[i-1],'\\');
+                removeChar(buff[i-1],'\\');
+            return i;
         }
-        return MAX_TOKENS;
+        //replace the space with a endstring
+        *buff[i] = '\0';
+        //move over the endstring
+        buff[i] += 1;
+        //test if string is empty
+        if(strcmp(buff[i-1],"") == 0){
+            buff[i-1] = buff[i];
+            i -= 1;
+        }
+        removeChar(buff[i-1],'\\');
+    }
+    return MAX_TOKENS;
 }
 
 
-bool match_command(struct CommandBuffer * cb,char ** cwd,const char ** path){
-	if(strcmp(cb->command_buffer,"exit") == 0){
-		return false;
-	}else{
-		char * buff[MAX_TOKENS];
-		int amount_tokens = parse_command(buff,cb->command_buffer," ");
-                for(int i = 0;i < amount_tokens;i++){
-                    printf("test: %s \n",buff[i]);
+char * command_full_path(char * buff,const char ** path,char * name){
+    size_t itt = 0;
+    while(path[itt]){
+        //get full path
+        strncpy(buff,path[itt],PATH_BUFFER_SIZE);
+        strncat(buff,name,PATH_BUFFER_SIZE);
+        //test if we can acces file
+        struct stat sb;
+        if(!stat(buff,&sb)){
+            if(S_ISREG(sb.st_mode) && sb.st_mode & 0111){
+                if(access(buff,X_OK) != -1){
+                    return buff;
+                }else if(!path[itt+1]){
+                    printf("%s\n",strerror(errno));
                 }
-                if(strcmp(buff[0],"cd") == 0){
-			int err = chdir(buff[1]);
-			if(err == -1){
-				printf("%s\n",strerror(errno));
-			}
-			(*cwd) = getcwd(*cwd,PATH_BUFFER_SIZE);
-		}else{
-                        size_t itt = 0;
-                        while(path[itt]){
-                            //get full path
-                            char full_path_buff[PATH_BUFFER_SIZE];
-                            strncpy(full_path_buff,path[itt],PATH_BUFFER_SIZE);
-                            strncat(full_path_buff,buff[0],PATH_BUFFER_SIZE);
-                            //test if we can acces file
-                            if(access(full_path_buff,X_OK) != -1){
-                                pid_t pid = fork();
-                                if(pid == 0){
-                                    if(execv(full_path_buff,&buff[0]) == -1)
-                                        printf("%s\n",strerror(errno));
-                                    return false;
-                                }else{
-                                    waitpid(pid,NULL,0);
-                                    return true;
-                                }
-                            }else if(!path[itt+1]){
-                                printf("%s\n",strerror(errno));
-                            }
-                            itt += 1;
-                        }
-                }
-                return true;
+            }
         }
+        ++itt;
+    }
+    return NULL;
+}
+
+bool match_command(struct CommandBuffer * cb,char ** cwd,const char ** path){
+    if(strcmp(cb->command_buffer,"exit") == 0){
+        return false;
+    }else{
+        char * buff[MAX_TOKENS];
+        parse_command(buff,cb->command_buffer," ");
+        if(strcmp(buff[0],"cd") == 0){
+            int err = chdir(buff[1]);
+            if(err == -1){
+                printf("%s\n",strerror(errno));
+            }
+            (*cwd) = getcwd(*cwd,PATH_BUFFER_SIZE);
+        }else{
+            bool found_pipe = false;
+            size_t pos = 0;
+            while(buff[pos]){
+                if(strcmp(buff[pos],"|") == 0){
+                    found_pipe = true;
+                    buff[pos] = NULL;
+                    break;
+                }
+                ++pos;
+            }
+            if(found_pipe){
+                //get full path
+                char full_path_buff[PATH_BUFFER_SIZE];
+                char * full_path = command_full_path(full_path_buff,path,buff[0]);
+                if(!full_path){
+                    return true;
+                }
+                pid_t pid = fork();
+                {
+                    if(pid == -1){
+                        printf("%s\n",strerror(errno));
+                    }
+                    if(pid == 0){//parent
+                        char sub_buff[PATH_BUFFER_SIZE];
+                        char * sub_full_path = command_full_path(sub_buff,path,buff[pos+1]);
+                        if(!sub_full_path){
+                            exit(1);
+                        }
+                        int pipes[2];
+                        if(pipe(pipes) == -1){
+                            printf("%s\n",strerror(errno));
+                            exit(1);
+                        }
+                        pid_t pid2 = fork();
+                        {
+                            if(pid2 == 0){//CHILD
+                                close(pipes[1]);
+                                while ((dup2(pipes[0],0) == -1) && (errno == EINTR)) {}
+                                close(pipes[0]);
+                                if(execv(sub_full_path,&buff[pos+1]) == -1)
+                                    printf("%s\n",strerror(errno));
+                                exit(0);
+                            }else{ //PARENT
+                                close(pipes[0]);
+                                while ((dup2(pipes[1], STDOUT_FILENO) == -1) && (errno == EINTR)) {}
+                                close(pipes[1]);
+                                if(execv(full_path,&buff[0]) == -1)
+                                    printf("%s\n",strerror(errno));
+                                wait(NULL);
+                                exit(0);
+                            }
+                        }
+                    }else{//grand parent
+                        wait(NULL);
+                        return true;
+                    }
+                }
+            }else{
+                char full_path_buff[PATH_BUFFER_SIZE];
+                char * full_path = command_full_path(full_path_buff,path,buff[0]);
+                if(!full_path){
+                    return true;
+                }
+                pid_t pid = fork();
+                if(pid == 0){
+                    if(execv(full_path,&buff[0]) == -1)
+                        printf("%s\n",strerror(errno));
+                    return false;
+                }else{
+                    waitpid(pid,NULL,0);
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
 }
 
 /*
